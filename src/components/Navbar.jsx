cecec35eb3a14db5043abb9298d835164539ab15
Bijesh Neupane <neubij64@gmail.com>
2025-01-13 16:00:55 +0545

    changed the name of pdf so that it works fine in vercel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -77,7 +77,7 @@ const Navbar = () => {
           </a>
           <a
             className="flex-1 w-full flex items-center justify-center gap-8 text-4xl bg-gray-800 hover:bg-gray-700"
-            href="/react php.pdf"
+            href="/react_php.pdf"
             download
           >
             <img
