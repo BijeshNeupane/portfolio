8abee7324e30ae5867372aabb0252ebb42830f4f
Bijesh Neupane <neubij64@gmail.com>
2025-04-30 20:50:07 +0545

    added 3d model of myself

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,25 +5,32 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./desktop_pc/model.glb");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
+      <ambientLight intensity={0.3} />
+      <directionalLight
+        position={[5, 10, 5]}
         intensity={1}
         castShadow
-        shadow-mapSize={1024}
+        shadow-mapSize-width={2024}
+        shadow-mapSize-height={2024}
       />
-      <pointLight intensity={1} />
+      <spotLight
+        position={[0, 15, 10]}
+        angle={0.3}
+        penumbra={1}
+        intensity={1.2}
+        castShadow
+      />
+      <pointLight intensity={0.5} />
+
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 2.4 : 3}
+        position={isMobile ? [0, -3.35, 0] : [0, -4, 0]}
+        rotation={[0, 0, 0]}
       />
     </mesh>
   );
