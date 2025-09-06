195f8de75d24a4f99801c2064ec46dbcdadc4df8
BijeshNeupane <bijesh.neupane@volgai.com>
2025-09-06 13:42:05 +0545

    Implement code changes to enhance functionality and improve performance

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -118,7 +118,7 @@ const Contact = () => {
               {loading ? "Sending..." : "Send"}
             </button>
             <a
-              href="./resume.pdf"
+              href="./Resume.pdf"
               download
               className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
             >
