d77e75cc1b7d1eae9e61a458cd68761ea90c796a
BijeshNeupane <bijesh.neupane@volgai.com>
2025-09-06 13:38:00 +0545

    fix: update resume link path in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -118,7 +118,7 @@ const Contact = () => {
               {loading ? "Sending..." : "Send"}
             </button>
             <a
-              href="/resume.pdf"
+              href="./resume.pdf"
               download
               className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
             >
