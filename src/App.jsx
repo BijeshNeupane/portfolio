7c7d85801b34fc5c5f1f4c085da585c75cdb47d8
BijeshNeupane <neubij64@gmail.com>
2025-10-14 15:17:41 +0545

    Added a chatbot using n8n

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,22 @@ import {
   Works,
   StarsCanvas,
 } from "./components";
+import { useEffect } from "react";
+import "@n8n/chat/style.css";
+import { createChat } from "@n8n/chat";
 
 const App = () => {
+  useEffect(() => {
+    createChat({
+      webhookUrl:
+        "https://bijesh53.app.n8n.cloud/webhook/dd3b2486-6cf2-4cfd-989c-27ce565bb0dc/chat",
+      initialMessages: [
+        "Hi there! 👋",
+        "I am Bijesh's bot. How can I assist you today?",
+        "You can ask me about Bijesh's projects, skills, or experience.",
+      ],
+    });
+  }, []);
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
